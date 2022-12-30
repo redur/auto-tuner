f4d3420e41f573d249a3cccc4849c116c6251a30
Renato Durrer <rd@visium.ch>
2022-12-30 15:15:44 +0100

    Remove unused files

--- src/data_extraction.py
+++
@@ -1,9 +0,0 @@
-"""
-Created on Wed Sep 12 2018
-
-@author: Fabien Tarrade fabien.tarrade@axa.ch
-"""
-
-import numpy as np
-import pandas as pd
-
--- src/preprocessing.py
+++
@@ -1,6 +0,0 @@
-"""
-Created on Wed Sep 12 2018
-
-@author: Fabien Tarrade fabien.tarrade@axa.ch
-"""
-
